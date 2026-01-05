b2e1e4f56c8f3f388c9857e24397bfb76f479c22
Smart Cloud Solutions Inc. <info@smart-cloud-solutions.com>
2026-01-05 16:52:48 +0100

    Dependency update

--- wpsuite-admin/dist/index.asset.php
+++ wpsuite-admin/dist/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-i18n'), 'version' => '673c3f231b7935012ac6');
+<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-i18n'), 'version' => '471d93d5c58621bfbd7e');
